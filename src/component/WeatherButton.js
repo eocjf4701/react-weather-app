1ec654c0ae4c0a0a74e1ecc8f7a0e76a0183c5a2
parkdaechul <eocjf4701@gmail.com>
2023-03-23 16:43:42 +0900

    화면 component css 수정

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 
 const WeatherButton = ({ cities, setCity }) => {
   return (
-    <div>
+    <div className="region-button">
       <Button variant="warning">Current Location</Button>
       {cities.map((item, index) => (
         < Button variant="warning" key={index} onClick={() => setCity(item)}> {item}</Button>
