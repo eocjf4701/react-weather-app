5b9270e457cb9de77cc54e89a64c86fbed53b331
parkdaechul <eocjf4701@gmail.com>
2023-03-26 14:08:30 +0900

    다른 나라 클릭 후에 현재위치 선택시 반응 및 mouse hover css 추가

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 const WeatherButton = ({ cities, setCity }) => {
   return (
     <div className="region-button">
-      <Button variant="warning">Current Location</Button>
+      <Button variant="warning" onClick={() => setCity('')}>Current Location</Button>
       {cities.map((item, index) => (
         < Button variant="warning" key={index} onClick={() => setCity(item)}> {item}</Button>
       ))}
