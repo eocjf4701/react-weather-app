b54d782c78f570351920e6d646b889bf89474ef2
parkdaechul <eocjf4701@gmail.com>
2023-03-26 13:51:21 +0900

    로딩 스피너 만들기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
+import ClipLoader from "react-spinners/ClipLoader";
 
 // 1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
 // 2. 날씨정보에는 도시, 섭씨, 화씨 날씨상태
@@ -14,6 +15,7 @@ import WeatherButton from './component/WeatherButton';
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
   const cities = ['paris', 'new york', 'tokyo', 'seoul'];
   // 1.
   const getCurrentLocation = () => {
@@ -27,18 +29,22 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e11ffa8a077b9e1f13e6816563995de0&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
 
     setWeather(data);
+    setLoading(false);
   }
 
   const getWeatherByCity = async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e11ffa8a077b9e1f13e6816563995de0&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
 
     setWeather(data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -48,10 +54,16 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
-      </div>
+      {loading ?
+        <div className="container">
+          <ClipLoader color='f88c6b' loading={loading} size={150} />
+        </div>
+        :
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      }
     </>
   );
 }
